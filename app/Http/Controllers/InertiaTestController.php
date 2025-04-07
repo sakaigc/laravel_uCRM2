8b7c7bb8ac3c5f6a63d68537367f2e1498ee651d
sakaigc <sakai.gc01@gmail.com>
2025-04-07 10:04:02 +0900

    flash_message

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -31,7 +31,10 @@ class InertiaTestController extends Controller
         $inertiaTest->title = $request->title;
         $inertiaTest->content = $request->content;
         $inertiaTest->save();
-        return to_route('inertia.index');
+        return to_route('inertia.index')
+        ->with([
+        'message' => '登録しました。'
+        ]); 
     }
 
     public function create()
--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -2,4 +2,7 @@
 </script>
 <template>
 Inertiaテストです。<br>
+<div v-if="$page.props.flash.message">
+ {{ $page.props.flash.message }}
+</div>
 </template>
